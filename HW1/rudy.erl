8e413854ac4bfb0a3b89a8f5316e9d77131eacf3
Malte Berg <malte@malte.nu>
2025-09-08 18:42:08 +0200

    Implement simple server functionality for Task 2.2 of HW1.

--- HW1/rudy.erl
+++ HW1/rudy.erl
@@ -1,13 +1,22 @@
 -module(rudy).
 -export([init/1]).
+% -- Task 2.2 Start --
+-export([start/1, stop/0]).
+
+start(Port) ->
+    register(rudy, spawn(fun() -> init(Port) end)).
+
+stop() ->
+    exit(whereis(rudy), "time to die").
+% -- Task 2.2 End --
 
 init(Port) ->
     Opt = [list, {active, false}, {reuseaddr, true}],
     case gen_tcp:listen(Port, Opt) of
         {ok, Listen} ->
-            % ---
+            % -- Task 2.1 Start --
             handler(Listen),
-            % ---
+            % -- Task 2.1 End --
             gen_tcp:close(Listen),
             ok;
         {error, _Error} ->
@@ -17,9 +26,12 @@ init(Port) ->
 handler(Listen) ->
     case gen_tcp:accept(Listen) of
         {ok, Client} ->
-            % ---
-            request(Client);
-            % ---
+            % -- Task 2.1 Start --
+            request(Client),
+            % -- Task 2.2 Start --
+            handler(Listen);
+            % -- Task 2.2 End --
+            % -- Task 2.1 End --
         {error, _Error} ->
             error
     end.
@@ -28,9 +40,9 @@ request(Client) ->
     Recv = gen_tcp:recv(Client, 0),
     case Recv of
         {ok, Str} ->
-            % ---
+            % -- Task 2.1 Start --
             Request = http:parse_request(Str),
-            % ---
+            % -- Task 2.1 End --
             Response = reply(Request),
             gen_tcp:send(Client, Response);
         {error, Error} ->
@@ -39,4 +51,7 @@ request(Client) ->
     gen_tcp:close(Client).
 
 reply({{get, URI, _}, _, _}) ->
+    % -- Task 2.1 Start -- 
     http:ok(URI).
+    % -- Task 2.1 End --
+

245f6a43c165859d058f9d622a3e06112467fe36
Malte Berg <malte@malte.nu>
2025-09-10 13:20:24 +0200

    Extend test.erl to have a way of benching with multiple processes/clients.

--- HW1/test.erl
+++ HW1/test.erl
@@ -1,11 +1,22 @@
 -module(test).
 -export([bench/2]).
+% For multiple clients
+-export([bench/3]).
 
 bench(Host, Port) ->
     Start = erlang:system_time(micro_seconds),
     run(100, Host, Port),
     Finish = erlang:system_time(micro_seconds),
-    Finish - Start.
+    io:format("~w~n", [Finish - Start]).
+
+% -- Run multiple benchmark processes --
+
+bench(_, _, 0) -> 
+    io:fwrite("Spawned!~n"),
+    ok;
+bench(Host, Port, Clients) ->
+    spawn(fun() -> bench(Host, Port) end),
+    bench(Host, Port, Clients-1).
 
 run(N, Host, Port) ->
     if 

7f1466413812e163b56da85811c28740afd4a029
Malte Berg <malte@malte.nu>
2025-09-15 14:46:53 +0200

    Implement update/4 with comments about benefits of entry/2 implementation.

--- HW2/dijkstra.erl
+++ HW2/dijkstra.erl
@@ -1,5 +1,5 @@
 -module(dijkstra).
--export([entry/2, replace/4]).
+-export([entry/2, replace/4, update/4]).
 
 entry(Node, Sorted) ->
     case lists:keyfind(Node, 1, Sorted) of
@@ -15,5 +15,15 @@ replace(Node, N, Gateway, Sorted) ->
             error;
         _ ->
             lists:sort(fun(A, B) -> entry(element(1,A), Sorted) > entry(element(1,B), Sorted) end,lists:keyreplace(Node, 1, Sorted, {Node, N, Gateway})) % > is used, reverse logic
+    end.
+
+% Thanks to entry returning 0 if node not in list, case will be false if node does not exist, meaning no need for an extra check.
+% It also catches if an empty list is provided.
+update(Node, N, Gateway, Sorted) ->
+    case N < entry(Node, Sorted) of
+        true ->
+            replace(Node, N, Gateway, Sorted);
+        _ ->
+            Sorted
     end.
 

c309e21f2405e4f152ae1a9f56650e4bf64796e7
Malte Berg <malte@malte.nu>
2025-09-15 22:15:34 +0200

    Implement new/1 and update/3 for hist module, subject to change.

--- HW2/hist.erl
+++ HW2/hist.erl
@@ -1,2 +1,21 @@
 -module(hist).
 -export([new/1, update/3]).
+
+new(Name) ->
+    [{Name, inf}].
+
+update(Node, N, History) ->
+    case lists:keyfind(Node, 1, History) of
+        {Node, Counter} ->
+            if 
+                N > Counter ->
+                    Updated = lists:keyreplace(Node, 1, History, {Node, N}),
+                    {new, Updated};
+                true ->
+                    old
+            end;
+        _ ->
+            Updated = [{Node, N} | History],
+            {new, Updated}
+    end.
+

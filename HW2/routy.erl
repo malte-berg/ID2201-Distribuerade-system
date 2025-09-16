25f923ccbb55ec293abc1ae7a86b3964c746c989
Malte Berg <malte@malte.nu>
2025-09-16 11:37:37 +0200

    Add skeleton code provided in instructions for routy.erl.

--- HW2/routy.erl
+++ HW2/routy.erl
@@ -15,3 +15,34 @@ init(Name) ->
     Hist = hist:new(Name),
     router(Name, 0, Hist, Intf, Table, Map).
 
+router(Name, N, Hist, Intf, Table, Map) ->
+    receive
+        % :
+        % :
+        {add, Node, Pid} ->
+            Ref = erlang:monitor(process, Pid),
+            Intf1 = intf:add(Node, Ref, Pid, Intf),
+            router(Name, N, Hist, Intf1, Table, Map);
+
+        {remove, Node} ->
+            {ok, Ref} = intf:ref(Node, Intf),
+            erlang:demonitor(Ref),
+            Intf1 = intf:remove(Node, Intf),
+            router(Name, N, Hist, Intf1, Table, Map);
+
+        {’DOWN’, Ref, process, _, _} ->
+            {ok, Down} = intf:name(Ref, Intf),
+            io:format("~w: exit recived from ~w~n", [Name, Down]),
+            Intf1 = intf:remove(Down, Intf),
+            router(Name, N, Hist, Intf1, Table, Map);
+        % :
+        % :
+        
+        {status, From} ->
+            From ! {status, {Name, N, Hist, Intf, Table, Map}},
+            router(Name, N, Hist, Intf, Table, Map);
+
+        stop ->
+            ok
+    end.
+

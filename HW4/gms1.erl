5af6df78d9740f94429609cef6998f5e17853f4b
Malte Berg <malte@malte.nu>
2025-09-27 15:55:38 +0200

    Add leader/4 and slave/5 to gms1 module.

---
+++ HW4/gms1.erl
@@ -0,0 +1,42 @@
+-module(gms1).
+
+-compile(export_all).
+
+leader(Id, Master, Slaves, Group) ->
+    receive
+        {mcast, Msg} ->
+            bcast(Id, {msg, Msg}, Slaves),
+            Master ! Msg,
+            leader(Id, Master, Slaves, Group);
+        {join, Wrk, Peer} ->
+            Slaves2 = lists:append(Slaves, [Peer]),
+            Group2 = lists:append(Group, [Wrk]),
+            bcast(Id, {view, [self() | Slaves2], Group2}, Slaves2),
+            Master ! {view, Group2},
+            leader(Id, Master, Slaves2, Group2);
+        stop ->
+            ok
+    end.
+
+bcast(_Id, Msg, Slaves) ->
+    Bcast = fun(Slave) -> Slave ! Msg end,
+    lists:foreach(Bcast, Slaves).
+
+slave(Id, Master, Leader, Slaves, Group) ->
+    receive
+        {mcast, Msg} ->
+            Leader ! {mcast, Msg},
+            slave(Id, Master, Leader, Slaves, Group);
+        {join, Wrk, Peer} ->
+            Leader ! {join, Wrk, Peer},
+            slave(Id, Master, Leader, Slaves, Group);
+        {msg, Msg} ->
+            Master ! Msg,
+            slave(Id, Master, Leader, Slaves, Group);
+        {view, [Leader | Slaves2], Group2} ->
+            Master ! {view, Group2}
+            slave(Id, Master, Leader, Slaves2, Group2);
+        stop ->
+            ok
+    end.
+

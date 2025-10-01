ed506ab16908d5508408c14bd910ed6154f8db80
Malte Berg <malte@malte.nu>
2025-10-01 14:45:07 +0200

    Create demo module.

---
+++ HW4/demo.erl
@@ -0,0 +1,15 @@
+-module(demo).
+
+-export([demo/0]).
+
+demo() ->
+    W1 = test:first(1, gms3, 1000),
+    timer:sleep(50),
+    W2 = test:add(2, gms3, W1, 1000),
+    timer:sleep(50),
+    loop(3, W2).
+
+loop(N, Prev) ->
+    timer:sleep(4000),
+    New = test:add(N, gms3, Prev, 1000),
+    loop(N+1, New).

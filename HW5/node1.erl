235ae96da952403f9b700c59ed8ee4445c32fb3f
Malte Berg <malte@malte.nu>
2025-10-05 11:44:18 +0200

    Implement initial node1 module containing message catching.

---
+++ HW5/node1.erl
@@ -0,0 +1,17 @@
+-module(node1).
+
+node(Id, Predecessor, Successor) ->
+    receive
+        {key, Qref, Peer} ->
+            Peer ! {Qref, Id},
+            node(Id, Predecessor, Successor);
+        {notify, New} ->
+            Pred = notify(New, Id, Predecessor),
+            node(Id, Pred, Successor);
+        {request, Peer} ->
+            request(Peer, Predecessor),
+            node(Id, Predecessor, Successor);
+        {status, Pred} ->
+            Succ = stabilize(Pred, Id, Successor),
+            node(Id, Predecessor, Succ)
+    end.

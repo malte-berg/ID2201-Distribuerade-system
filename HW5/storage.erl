d0ede37e15c4b2c110127637f1f51a650994ffdf
Malte Berg <malte@malte.nu>
2025-10-07 21:43:35 +0200

    Create module storage and outline functions to be implemented.

---
+++ HW5/storage.erl
@@ -0,0 +1,17 @@
+-module(storage).
+
+create() ->
+    ok.
+
+add(Key, Value, Store) ->
+    ok.
+
+lookup(Key, Store) ->
+    ok.
+
+split(From, To, Store) ->
+    ok.
+
+merge(Entries, Store) ->
+    ok.
+
